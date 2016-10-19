2509c4bcc1c98372bf7ee1aae761c0d99a88fa07
“John <johnjamesmartin@live.com>
2016-10-19 03:14:15 +0100

    Tutorial 1 - Introduction

---
+++ general.py
@@ -0,0 +1,18 @@
+# Imports
+
+import os
+
+
+# Did we create folder? If not, create it:
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+# Write file:
+
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
