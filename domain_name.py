cd9f0754640c0565bceb2b5035a363996c34c7e7
“John <johnjamesmartin@live.com>
2016-10-19 23:50:52 +0100

    Tutorial 2 - Top-Level Domain Name

---
+++ domain_name.py
@@ -0,0 +1,14 @@
+# Imports
+
+# tld = Top-Level Domain extractor package:
+
+from tld import get_tld
+
+
+# Get Top-Level Domain from URL:
+
+def get_domain_name(url):
+    domain_name = get_tld(url)
+    return domain_name
+
+# print(get_domain_name('http://www.thenewboston.com/'))
