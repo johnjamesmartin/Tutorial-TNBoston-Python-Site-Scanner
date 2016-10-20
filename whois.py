bb2da208259818572b3dfafad11e902bc0a373ff
“John <johnjamesmartin@live.com>
2016-10-20 01:48:02 +0100

    Tutorial 5 - Whois

---
+++ whois.py
@@ -0,0 +1,13 @@
+# Imports:
+
+import os
+
+
+def get_whois(url):
+    command = "whois " + url
+    process = os.popen(command)
+    results = str(process.read())
+    return results
+
+
+# print(get_whois('thenewboston.com'))
